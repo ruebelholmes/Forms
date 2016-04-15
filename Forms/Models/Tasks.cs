721f453da62ab31c524f5351453fe62f6b32dd06
Ruebel Holmes <treyholmes@yahoo.com>
2016-04-15 10:27:06 -0500

    Initial commit

---
+++ Forms/Models/Tasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forms.Models
+{
+    public class Tasks
+    {
+        public int ID { get; set; }
+        public string CreatedBy { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
